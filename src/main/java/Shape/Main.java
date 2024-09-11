83314c665bc71f1a73da088e054e0eb428d0ef34
Mahirul Alam Chowdhury <62341446+Slyyoursenpai@users.noreply.github.com>
2024-09-11 17:21:52 +0600

    Second commit (changed main class codes)

--- src/main/java/Shape/Main.java
+++ src/main/java/Shape/Main.java
@@ -19,6 +19,7 @@ public class Main {
         printShapeDetails(circle);
         printShapeDetails(rectangle);
 
+        ////testing setters
         rectangle.setHeight(2);
         printShapeDetails(rectangle);
 
